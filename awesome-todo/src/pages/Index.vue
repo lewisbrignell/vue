4e2af45f4f4ee63a3fc5d68f9bf06b8904f2a7a4
Lewis Brignell <lbrignell@angus.lan>
2020-09-28 16:59:15 -0400

    todo app functionality added

--- awesome-todo/src/pages/Index.vue
+++ awesome-todo/src/pages/Index.vue
@@ -1,14 +1,70 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page padding>
+    <button
+      style="position: absolute; right: 10px;"
+      @click="counter++">
+      {{ counter }}
+    </button>
+
+    <input
+      v-model="message"
+      @keyup.esc="clearMessage"
+      @keyup.enter="alertMessage"
+      v-autofocus />
+
+    <button @click="clearMessage">Clear</button>
+
+    <h5
+      v-if="message.length"
+      class="border-grey">{{ message }}</h5>
+    <h6 v-else>No Message Entered</h6>
+
+    <hr>
+
+    <p>Uppercase message: {{ messageUppercase }}</p>
+    <p>Lowercase message: {{ message | messageLowerCase }}</p>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  data () {
+    return {
+      message: 'I love Vue.js so much',
+      counter: 0
+    }
+  },
+  computed: {
+    messageUppercase () {
+      console.log('messageUppercase fired')
+      return this.message.toUpperCase()
+    }
+  },
+  methods: {
+    clearMessage () {
+      this.message = ''
+    },
+    alertMessage () {
+      alert(this.message)
+    }
+  },
+  filters: {
+    messageLowerCase (value) {
+      return value.toLowerCase()
+    }
+  },
+  directives: {
+    autofocus: {
+      inserted (el) {
+        el.focus()
+      }
+    }
+  }
 }
 </script>
+
+<style>
+  .border-grey {
+    border: 1px solid grey;
+  }
+</style>
